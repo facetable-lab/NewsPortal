e3d36b3156af8d22a685595f3f52631d9c634740
facetable-lab <diofff6@gmail.com>
2023-01-31 15:10:17 +0300

    Функция отображения главной страницы

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # print(request)
+    return HttpResponse('Hello world')
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
