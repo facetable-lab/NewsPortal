5c2cedd75468209e1a133dc620f1a8fb846e3a49
facetable-lab <diofff6@gmail.com>
2023-02-01 23:31:36 +0300

    Модели категории для новостей

--- news/models.py
+++ news/models.py
@@ -1,13 +1,28 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+        ordering = ['title']
+
+
 class News(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Заголовок')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
     image = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True, verbose_name='Изображение')
-    is_published = models.BooleanField(default=False, verbose_name='Опубликованно')
+    is_published = models.BooleanField(default=False, db_index=True, verbose_name='Опубликованно')
+
+    category = models.ForeignKey(Category, null=True, on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.title
