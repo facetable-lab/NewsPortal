3b1c1561993dac5a031b04f526e0159c89a4a891
facetable-lab <diofff6@gmail.com>
2023-02-01 05:17:11 +0300

    Рендер главной страницы с передачей данных

--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import News
 
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
 
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    context = {
+        'title': 'Список новостей',
+        'news': news
+    }
+
+    return render(request, template_name='news/index.html', context=context)
