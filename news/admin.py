a8aa89539dabadba47f00581cca2a3236926596b
facetable-lab <diofff6@gmail.com>
2023-02-01 23:31:22 +0300

    Отображение модели категории

--- news/admin.py
+++ news/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'created_at', 'updated_at', 'is_published')
-    list_display_links = ('pk', 'title')
+    list_display = ('pk', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('pk', 'title', 'category')
     search_fields = ('title', 'content')
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pk')
+    list_display_links = ('title', 'pk')
+    search_fields = ('title',)
+
+
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
