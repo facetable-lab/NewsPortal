154001c86e252aab3b5656e5d6675aa260f91697
facetable-lab <diofff6@gmail.com>
2023-02-02 22:13:13 +0300

    Добавлены имена маршрутов

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, get_category
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category)
+    path(route='', view=index, name='home'),
+    path(route='category/<int:category_id>/', view=get_category, name='category')
 ]
