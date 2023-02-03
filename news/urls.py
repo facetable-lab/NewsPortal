772e6ee845c0edb3ead79cac7a88c9dc29be10cc
facetable-lab <diofff6@gmail.com>
2023-02-03 21:33:10 +0300

    Путь к конкретной новости

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index, get_category
+from .views import index, get_category, view_news
 
 urlpatterns = [
     path(route='', view=index, name='home'),
-    path(route='category/<int:category_id>/', view=get_category, name='category')
+    path(route='category/<int:category_id>/', view=get_category, name='category'),
+    path(route='news/<int:news_id>', view=view_news, name='view_news')
 ]
