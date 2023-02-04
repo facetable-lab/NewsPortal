3a001507221e869823f4eaae5e34f926186f5ef4
facetable-lab <diofff6@gmail.com>
2023-02-04 05:14:14 +0300

    Украшение формы

--- news/forms.py
+++ news/forms.py
@@ -4,7 +4,23 @@ from .models import Category
 
 
 class AddNewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, widget=forms.TextInput(
+        attrs={
+            'class': 'form-control'
+        }
+    ), label='Заголовок')
+
+    content = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'rows': 15
+        }
+    ), label='Текст')
+
+    is_published = forms.BooleanField(required=False, initial=True, label='Опубликовать сразу')
+
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.Select(
+        attrs={
+            'class': 'form-control'
+        }
+    ), empty_label='Выберите категорию', label='Категория')
