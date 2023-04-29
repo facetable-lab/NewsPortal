1033990af5aa56f4f02e3b84ff83942e6213f137
facetable-lab <diofff6@gmail.com>
2023-04-29 05:27:42 +0300

    На отладочном сервере изображения идут в директорию media

--- news_portal/urls.py
+++ news_portal/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
-from news import views
+from news_portal.settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
